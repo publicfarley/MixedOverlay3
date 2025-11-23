b4370462ff142f8b3d09f1e6f2ea1e514f20b69d
publicfarley <publicfarley@gmail.com>
2025-11-23 18:45:09 -0500

    Initial Commit

---
+++ MixedOverlay3/MixedOverlay3App.swift
@@ -0,0 +1,17 @@
+//
+//  MixedOverlay3App.swift
+//  MixedOverlay3
+//
+//  Created by Farley Caesar on 2025-11-23.
+//
+
+import SwiftUI
+
+@main
+struct MixedOverlay3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
